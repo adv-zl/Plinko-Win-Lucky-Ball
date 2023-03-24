cf8c630c748d1fb15401832ee7f96c064f2cc046
Olexiy Trushkovsky <trushkovskya@gmail.com>
2023-03-24 16:59:13 +0200

    working

--- dist/js/261.1d211c2e.js
+++ dist/js/261.1d211c2e.js
@@ -3,4 +3,4 @@
  * (C) Ionic http://ionicframework.com - MIT License
  */
 const r=()=>{document.addEventListener("backbutton",(()=>{}))},o=()=>{const t=document;let n=!1;t.addEventListener("backbutton",(()=>{if(n)return;let e=0,r=[];const o=new CustomEvent("ionBackButton",{bubbles:!1,detail:{register(t,n){r.push({priority:t,handler:n,id:e++})}}});t.dispatchEvent(o);const i=async t=>{try{if(null===t||void 0===t?void 0:t.handler){const n=t.handler(a);null!=n&&await n}}catch(n){console.error(n)}},a=()=>{if(r.length>0){let t={priority:Number.MIN_SAFE_INTEGER,handler:()=>{},id:-1};r.forEach((n=>{n.priority>=t.priority&&(t=n)})),n=!0,r=r.filter((n=>n.id!==t.id)),i(t).then((()=>n=!1))}};a()}))},i=100,a=99}}]);
-//# sourceMappingURL=261.9517bab8.js.map
+//# sourceMappingURL=261.1d211c2e.js.map
--- dist/js/78.29ea76da.js
+++ dist/js/78.29ea76da.js
@@ -1,9 +1,9 @@
 "use strict";(self["webpackChunkonboarding"]=self["webpackChunkonboarding"]||[]).push([[78],{8487:(o,t,e)=>{e.d(t,{a:()=>u,c:()=>m,g:()=>l,s:()=>h});var n=e(6587);
 /*!
  * (C) Ionic http://ionicframework.com - MIT License
- */const r="ION-CONTENT",s="ion-content",a=".ion-content-scroll-host",c=`${s}, ${a}`,i=o=>o&&o.tagName===r,l=async o=>i(o)?(await new Promise((t=>(0,n.c)(o,t))),o.getScrollElement()):o,u=o=>o.closest(c),h=(o,t)=>{if(i(o)){const e=o;return e.scrollToTop(t)}return Promise.resolve(o.scrollTo({top:0,left:0,behavior:t>0?"smooth":"auto"}))},m=(o,t,e,n)=>{if(i(o)){const r=o;return r.scrollByPoint(t,e,n)}return Promise.resolve(o.scrollBy({top:e,left:t,behavior:n>0?"smooth":"auto"}))}},6078:(o,t,e)=>{e.r(t),e.d(t,{startStatusTap:()=>a});var n=e(65),r=e(8487),s=e(6587);
+ */const r="ION-CONTENT",s="ion-content",a=".ion-content-scroll-host",c=`${s}, ${a}`,i=o=>o.tagName===r,l=async o=>i(o)?(await new Promise((t=>(0,n.c)(o,t))),o.getScrollElement()):o,u=o=>o.closest(c),h=(o,t)=>{if(i(o)){const e=o;return e.scrollToTop(t)}return Promise.resolve(o.scrollTo({top:0,left:0,behavior:t>0?"smooth":"auto"}))},m=(o,t,e,n)=>{if(i(o)){const r=o;return r.scrollByPoint(t,e,n)}return Promise.resolve(o.scrollBy({top:e,left:t,behavior:n>0?"smooth":"auto"}))}},6078:(o,t,e)=>{e.r(t),e.d(t,{startStatusTap:()=>a});var n=e(65),r=e(8487),s=e(6587);
 /*!
  * (C) Ionic http://ionicframework.com - MIT License
  */
 const a=()=>{const o=window;o.addEventListener("statusTap",(()=>{(0,n.wj)((()=>{const t=o.innerWidth,e=o.innerHeight,a=document.elementFromPoint(t/2,e/2);if(!a)return;const c=(0,r.a)(a);c&&new Promise((o=>(0,s.c)(c,o))).then((()=>{(0,n.Iu)((async()=>{c.style.setProperty("--overflow","hidden"),await(0,r.s)(c,300),c.style.removeProperty("--overflow")}))}))}))}))}}}]);
-//# sourceMappingURL=78.f41e49c4.js.map
+//# sourceMappingURL=78.29ea76da.js.map
